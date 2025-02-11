d8b487e4b477680bbae19ad6d91ba2fed5966d45
Joseph Sankah <josephsankah0@gmail.com>
2025-02-11 13:21:21 -0500

    removed manual id field from Review model

--- amplify/data/models/Review.ts
+++ amplify/data/models/Review.ts
@@ -1,9 +1,9 @@
 import { a } from '@aws-amplify/backend';
 
 export const Review = a.model({
-  reviewId: a.id(),  //Unique identifier (matches Cognito sub).
-  businessID: a.id(),
-  userID: a.id(),
+  reviewId: a.string(),  //Unique identifier (matches Cognito sub).
+  businessID: a.string(),
+  userID: a.string(),
   rating: a.float(),
   content: a.string(),
   reviewDate: a.timestamp()
