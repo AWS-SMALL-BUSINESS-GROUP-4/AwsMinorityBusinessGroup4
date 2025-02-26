d4c1d2ddab5ecabccd36a1c0fae6fb1217f4d738
Chukwuemeka Ojih <emmy@chukwueekasmbp2.mynetworksettings.com>
2025-02-25 19:45:52 -0500

    database modifications

--- amplify/data/models/Review.ts
+++ amplify/data/models/Review.ts
@@ -1,10 +1,15 @@
 import { a } from '@aws-amplify/backend';
 
-export const Review = a.model({
-  reviewId: a.string(),  //Unique identifier (matches Cognito sub).
-  businessId: a.string(),
-  userId: a.string(),
-  rating: a.float(),
-  content: a.string(),
+export const Review = a.model({ 
+  businessId: a.id(),  // reference field to match Business identifier
+  business: a.belongsTo('Business', 'businessId'),
+  userId: a.id(),  // reference field to match User identifier
+  user: a.belongsTo('User', 'userId'),
+  rating: a.float().required(),
+  content: a.string().required(),
   reviewDate: a.timestamp()
-}).authorization((allow) => [allow.owner()]);
+}).authorization((allow) => [
+    allow.owner(), 
+    allow.guest().to(['read']),
+    allow.groups(['Admin']).to(['read', 'update', 'delete'])
+])
