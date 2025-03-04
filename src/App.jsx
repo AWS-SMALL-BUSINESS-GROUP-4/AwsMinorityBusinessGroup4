43d7f2ad9c7207caae6786e019a654c4ce388286
Eshunfelix <104322012+Eshunfelix@users.noreply.github.com>
2025-03-04 08:45:53 -0500

    deleted search result files from homepage feature branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
-import SearchResultPage from "./components/SearchResultPage";
+// import SearchResultPage from "./components/SearchResultPage";
 import "./App.css";
 
 function App() {
