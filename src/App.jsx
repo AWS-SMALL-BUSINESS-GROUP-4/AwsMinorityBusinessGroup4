d5bd31fbcf14d9afbf60248a8ac9c83cfc5f3715
Chukwuemeka Ojih <emmy@chukwueekasmbp2.mynetworksettings.com>
2025-03-04 16:12:45 -0500

    tested changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-// import SearchResultPage from "./components/SearchResultPage";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
