60a7d805c3425feb82a606596f918c91f97d4b04
jDixon0004 <jaelen.dixon@bison.howard.edu>
2025-03-11 08:34:13 -0400

    Added additional storage bucket and permissions

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,27 @@
 import { defineStorage } from '@aws-amplify/backend';
 
-export const storage = defineStorage({
-  name: 'AwsMinorityBusinessGroup4Drive'
-});
+export const firstBucket = defineStorage({
+    name: 'AWSMBG4-public',
+    isDefault: true, 
+    access: (allow) => ({
+        
+    })
+  });
+  
+  export const secondBucket = defineStorage({
+    name: 'AWSMBG4-private',
+    access: (allow) => ({
+      'businesses/{business_id}/*': [
+        allow.entity('identity').to(['read', 'write', 'delete'])
+      ],
+      'businesses/{business_id}/photos/{photo_id}': [
+        allow.guest.to(['read'])
+      ],
+      'users/{user_id}/*': [
+        allow.entity('identity').to(['read', 'write', 'delete'])
+      ],
+      'users/{user_id}/photos/{photo_id}': [
+        allow.guest.to(['read'])
+      ],
+    })
+  })
