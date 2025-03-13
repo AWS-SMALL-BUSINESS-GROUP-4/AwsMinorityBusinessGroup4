f467ffd90e90d1f7bc955d19192a15da2650ccf2
jDixon0004 <jaelen.dixon@bison.howard.edu>
2025-03-13 13:35:00 -0400

    Started WIP pictures page

---
+++ src/pages/BusinessManagementPicturesPage.jsx
@@ -0,0 +1,98 @@
+import { useState, useRef } from 'react';
+import reactLogo from '../assets/react.svg'
+import '../App.css'
+import '../components/ContainerStyles.css'
+import './BusinessManagementPage.css'
+import './BusinessManagementPicturePage.css'
+import '../components/TextStyles.css'
+import BusinessNavBar from '../components/BusinessNavBar'
+import BusinessManagementSidebar from '../components/BusinessManagementSideBar';
+
+function BusinessManagementPicturesPage() {
+
+    const outsideImgUpload = useRef(null);
+
+    const [images, setImages] = useState([
+        'https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1273516682.jpg?c=original',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRSnyNJ8vSZR-n2Ou81gUeoGvwgj-8XhyYBA&s',
+        'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg?w=1155&h=1528',
+        'https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1273516682.jpg?c=original',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRSnyNJ8vSZR-n2Ou81gUeoGvwgj-8XhyYBA&s',
+        'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg?w=1155&h=1528',
+        'https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1273516682.jpg?c=original',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRSnyNJ8vSZR-n2Ou81gUeoGvwgj-8XhyYBA&s',
+        'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg?w=1155&h=1528',
+        'https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1273516682.jpg?c=original',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRSnyNJ8vSZR-n2Ou81gUeoGvwgj-8XhyYBA&s',
+        'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg?w=1155&h=1528',
+    ]);
+
+    const handleDelete = (index) => {
+        //const confirm = window.confirm("Are you sure you want to delete this picture?");
+        if(confirm) {
+            // Delete the photo
+        }
+    }
+
+    const handleOutsideImgUpload = () => {
+        outsideImgUpload.current.click();
+    }
+
+
+    return (
+        <>
+            <BusinessNavBar/>
+            <div className='sidebar-page-container'>
+                <BusinessManagementSidebar/>
+                {/*Main*/}
+                <div className='main'>
+                    <h1>Pictures</h1>
+                    <hr/>
+                    <div>
+                        <div className='spread'>
+                            <h2>Outside</h2>
+                            <input type='file' ref={outsideImgUpload} accept='image/*' className='hidden'/>
+                            <button onClick={handleOutsideImgUpload}>Upload Image</button>
+                        </div>
+                        <div className='spread scroll'>
+                            {images.map((source, index) => (
+                                <div key={index} className="img-with-button"> 
+                                    <img src={source}/>
+                                    <button className="btn" onClick={handleDelete(index)}>Delete</button>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <hr/>
+                    <div>
+                        <div className='spread'>
+                            <h2>Inside</h2>
+                            <input type='file' ref={outsideImgUpload} accept='image/*' className='hidden'/>
+                            <button onClick={handleOutsideImgUpload}>Upload Image</button>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div>
+                        <div className='spread'>
+                            <h2>Menu Items</h2>
+                            <input type='file' ref={outsideImgUpload} accept='image/*' className='hidden'/>
+                            <button onClick={handleOutsideImgUpload}>Upload Image</button>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div>
+                        <div className='spread'>
+                            <h2>All Pictures</h2>
+                            <input type='file' ref={outsideImgUpload} accept='image/*' className='hidden'/>
+                            <button onClick={handleOutsideImgUpload}>Upload Image</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            
+        </>
+    );
+}
+
+export default BusinessManagementPicturesPage
