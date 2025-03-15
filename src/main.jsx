ad790fe9b5daaa92c7d1043c4605513516df65cc
Joseph Sankah <josephsankah0@gmail.com>
2025-03-15 01:31:07 -0400

    configured backend

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./App.css";
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
 
+// Configure Amplify
+Amplify.configure(outputs);
+
+// Render the app
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
