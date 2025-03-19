747fce802d77da33e031b1bd94d76e7212a1ea47
jDixon0004 <jaelen.dixon@bison.howard.edu>
2025-03-19 14:00:55 -0400

    Created Schema type for use in data access

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,9 +1,11 @@
-import { a, defineData } from '@aws-amplify/backend';
+import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
 import { Business } from './models/Business';
 import { Review } from './models/Review';
 import { User } from './models/User';
 
 const schema = a.schema({ Business, Review, User });
+
+export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
