f8a232c8d6fd14e49e702ad104423979cb976e29
Joseph Sankah <josephsankah0@gmail.com>
2025-03-24 01:36:29 -0400

    defined a standalone schema for businesHours

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,9 +1,9 @@
 import { a, defineData } from '@aws-amplify/backend';
-import { Business } from './models/Business';
+import { Business, BusinessHours } from './models/Business';
 import { Review } from './models/Review';
 import { User } from './models/User';
 
-const schema = a.schema({ Business, Review, User });
+const schema = a.schema({ Business, BusinessHours, Review, User });
 
 export const data = defineData({
   schema,
