64c8502e09c324d81cefa18b1287ad0b08fdc85a
jDixon0004 <jaelen.dixon@bison.howard.edu>
2025-04-14 10:00:31 -0400

    Updated to use Amplify UI to display images

--- src/pages/BusinessManagementPicturesPage.jsx
+++ src/pages/BusinessManagementPicturesPage.jsx
@@ -1,5 +1,5 @@
-import { useState, useRef } from 'react';
-import { uploadData } from 'aws-amplify/storage';
+import { useState, useRef, useEffect } from 'react';
+import { uploadData, getUrl, list } from 'aws-amplify/storage';
 import reactLogo from '../assets/react.svg'
 import '../App.css'
 import '../components/ContainerStyles.css'
@@ -8,12 +8,15 @@ import './BusinessManagementPicturePage.css'
 import '../components/TextStyles.css'
 import BusinessNavBar from '../components/BusinessNavBar'
 import BusinessManagementSidebar from '../components/BusinessManagementSideBar';
+import { StorageImage } from '@aws-amplify/ui-react-storage';
 import { useParams } from 'react-router-dom';
+import { generateClient } from 'aws-amplify/api';
 
 function BusinessManagementPicturesPage() {
 
     const outsideImgUpload = useRef(null);
-    const id = useParams().id;
+    const businessId = useParams().id;
+    const client = generateClient();
 
     const [images, setImages] = useState([
         'https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1273516682.jpg?c=original',
@@ -31,6 +34,37 @@ function BusinessManagementPicturesPage() {
     ]);
 
     const [newImg, setNewImg] = useState();
+    const [loading, setLoading] = useState(false);
+    const [paths, setPaths] = useState([]);
+
+    useEffect(() => {
+        async function fetchPictures() {
+            // setLoading(true);
+            // const response = await client.models.Business.get(
+            //     { id: businessId },
+            //     { selectionSet: ['photos'] },
+            // );
+            // if(response.errors) {
+            //     console.error("Error in retreiving business photos: ", response.errors);
+            // }
+            // else {
+            //     await setImages(response.data.photos);
+            // }
+            // setLoading(false);
+
+            const response = await list({
+                path: `business-photos/${businessId}/`,
+                options: {             
+                    // Specify a target bucket using name assigned in Amplify Backend
+                    bucket: 'AWSMBG4-private'
+                }
+            });
+
+            setPaths(response.items.map(item => item.path));
+        }
+
+        fetchPictures();
+    }, [businessId])
 
     const handleDelete = (index) => {
         //const confirm = window.confirm("Are you sure you want to delete this picture?");
@@ -43,28 +77,49 @@ function BusinessManagementPicturesPage() {
         outsideImgUpload.current.click();
     }
 
-    const handleFileUpload = (event) => {
+    const handleFileUpload = async (event) => {
         const file = event.target.files?.[0];
         if(file) {
-            setNewImg(file);
+            await setNewImg(file);
 
-            uploadData({
-                path: `business-photos/guest/${file.name}`,
+            const response = await uploadData({
+                path: `business-photos/${businessId}/${file.name}`,
                 data: newImg,
                 options: {             
                     // Specify a target bucket using name assigned in Amplify Backend
                     bucket: 'AWSMBG4-private'
                 }
-            });
+            }).result;
+
+            console.log("Result of uploading image: ", response.path);
+
+            setPaths([...paths, response.path]);
+
+            // const linkToStorageFile = await getUrl({
+            //     path: response.path,
+            // });
+
+            // console.log("New image url: ", linkToStorageFile.url.href);
+            // console.log("New images:", [linkToStorageFile.url.toString(), ...images]);
+
+            // const response2 = await client.models.Business.update({
+            //     id: businessId,
+            //     photos: [linkToStorageFile.url.toString(), ...images]
+            // });
+            
+            // await setImages([linkToStorageFile.url.href, ...images]);
+
+            // console.log("Result of database update: ", response2);
         }
     }
 
-
+    //console.log("Images:", images);
+    console.log("Paths before render:", paths);
     return (
         <>
             <BusinessNavBar/>
             <div className='sidebar-page-container'>
-                <BusinessManagementSidebar id={id}/>
+                <BusinessManagementSidebar id={businessId}/>
                 {/*Main*/}
                 <div className='main'>
                     <h1>Pictures</h1>
@@ -108,11 +163,14 @@ function BusinessManagementPicturesPage() {
                             <button className="btn" onClick={handleOutsideImgUpload}>Upload Image</button>
                         </div>
                         <div className='spread scroll'>
-                            {images.map((source, index) => (
+                            {/* {images.map((source, index) => (
                                 <div key={index} className="img-with-button"> 
                                     <img src={source}/>
                                     <button className="btn" onClick={handleDelete(index)}>Delete</button>
                                 </div>
+                            ))} */}
+                            {paths.map((source, index) => (
+                                <StorageImage key={index} alt="business photo" path={source}/>
                             ))}
                         </div>
                     </div>
