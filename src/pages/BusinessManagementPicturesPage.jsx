2f16a86b331050d0d247535f5eeffac10803448d
jDixon0004 <jaelen.dixon@bison.howard.edu>
2025-04-14 15:07:53 -0400

    Added/fixed image uploading, loading, and deletion

--- src/pages/BusinessManagementPicturesPage.jsx
+++ src/pages/BusinessManagementPicturesPage.jsx
@@ -1,6 +1,5 @@
 import { useState, useRef, useEffect } from 'react';
-import { uploadData, getUrl, list } from 'aws-amplify/storage';
-import reactLogo from '../assets/react.svg'
+import { uploadData, getUrl, remove } from 'aws-amplify/storage';
 import '../App.css'
 import '../components/ContainerStyles.css'
 import './BusinessManagementPage.css'
@@ -8,9 +7,9 @@ import './BusinessManagementPicturePage.css'
 import '../components/TextStyles.css'
 import BusinessNavBar from '../components/BusinessNavBar'
 import BusinessManagementSidebar from '../components/BusinessManagementSideBar';
-import { StorageImage } from '@aws-amplify/ui-react-storage';
 import { useParams } from 'react-router-dom';
 import { generateClient } from 'aws-amplify/api';
+import { StorageImage } from '@aws-amplify/ui-react-storage';
 
 function BusinessManagementPicturesPage() {
 
@@ -18,58 +17,56 @@ function BusinessManagementPicturesPage() {
     const businessId = useParams().id;
     const client = generateClient();
 
-    const [images, setImages] = useState([
-        'https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1273516682.jpg?c=original',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRSnyNJ8vSZR-n2Ou81gUeoGvwgj-8XhyYBA&s',
-        'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg?w=1155&h=1528',
-        'https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1273516682.jpg?c=original',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRSnyNJ8vSZR-n2Ou81gUeoGvwgj-8XhyYBA&s',
-        'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg?w=1155&h=1528',
-        'https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1273516682.jpg?c=original',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRSnyNJ8vSZR-n2Ou81gUeoGvwgj-8XhyYBA&s',
-        'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg?w=1155&h=1528',
-        'https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-1273516682.jpg?c=original',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRSnyNJ8vSZR-n2Ou81gUeoGvwgj-8XhyYBA&s',
-        'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg?w=1155&h=1528',
-    ]);
-
-    const [newImg, setNewImg] = useState();
-    const [loading, setLoading] = useState(false);
     const [paths, setPaths] = useState([]);
+
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         async function fetchPictures() {
-            // setLoading(true);
-            // const response = await client.models.Business.get(
-            //     { id: businessId },
-            //     { selectionSet: ['photos'] },
-            // );
-            // if(response.errors) {
-            //     console.error("Error in retreiving business photos: ", response.errors);
-            // }
-            // else {
-            //     await setImages(response.data.photos);
-            // }
-            // setLoading(false);
-
-            const response = await list({
-                path: `business-photos/${businessId}/`,
-                options: {             
-                    // Specify a target bucket using name assigned in Amplify Backend
-                    bucket: 'AWSMBG4-private'
-                }
-            });
-
-            setPaths(response.items.map(item => item.path));
+            setLoading(true);
+            const response = await client.models.Business.get(
+                { id: businessId },
+                { selectionSet: ['photos'] },
+            );
+            if(response.errors) {
+                console.error("Error in retreiving business photos: ", response.errors);
+            }
+            else {
+                await setPaths(response.data.photos);
+                console.log("Photos: ", response.data.photos);
+            }
+            setLoading(false);
         }
 
         fetchPictures();
     }, [businessId])
 
-    const handleDelete = (index) => {
-        //const confirm = window.confirm("Are you sure you want to delete this picture?");
+    const handleDelete = async (index) => {
+        const confirm = window.confirm("Are you sure you want to delete this picture?");
         if(confirm) {
-            // Delete the photo
+            try {
+                console.log(paths[index]);
+                const response = await remove({ 
+                    path: paths[index],
+                    bucket: 'AWSMBG4-private',
+                });
+
+                console.log("Result of removing file: ", response);
+
+                const response2 = await client.models.Business.update({
+                    id: businessId,
+                    photos: paths.filter(path => path !== paths[index])
+                });
+
+                console.log("Result of removing images from database: ", response2);
+
+                setPaths(paths.filter(image => image !== paths[index]));
+
+                console.log("Result of remove images: ", paths);
+
+            } catch (error) {
+                console.log('Error ', error);
+            }
         }
     }
 
@@ -79,42 +76,49 @@ function BusinessManagementPicturesPage() {
 
     const handleFileUpload = async (event) => {
         const file = event.target.files?.[0];
+
         if(file) {
-            await setNewImg(file);
-
-            const response = await uploadData({
-                path: `business-photos/${businessId}/${file.name}`,
-                data: newImg,
-                options: {             
-                    // Specify a target bucket using name assigned in Amplify Backend
-                    bucket: 'AWSMBG4-private'
-                }
-            }).result;
-
-            console.log("Result of uploading image: ", response.path);
-
-            setPaths([...paths, response.path]);
-
-            // const linkToStorageFile = await getUrl({
-            //     path: response.path,
-            // });
-
-            // console.log("New image url: ", linkToStorageFile.url.href);
-            // console.log("New images:", [linkToStorageFile.url.toString(), ...images]);
-
-            // const response2 = await client.models.Business.update({
-            //     id: businessId,
-            //     photos: [linkToStorageFile.url.toString(), ...images]
-            // });
+            try {
+                console.log("Uploading");
+                const response = await uploadData({
+                    path: `business-photos/${businessId}/${file.name}`,
+                    data: file,
+                    options: {
+                        bucket: 'AWSMBG4-private',
+                        preventOverwrite: true,
+                    }
+                }).result;
             
-            // await setImages([linkToStorageFile.url.href, ...images]);
 
-            // console.log("Result of database update: ", response2);
+                // const linkToStorageFile = await getUrl({
+                //     path: response.path,
+                // });
+
+                // const response2 = await client.models.Business.update({
+                //     id: businessId,
+                //     photos: [linkToStorageFile.url.toString(), ...paths]
+                // });
+
+                const response2 = await client.models.Business.update({
+                    id: businessId,
+                    photos: [response.path, ...paths]
+                });
+                
+                //await setPaths([linkToStorageFile.url.href, ...paths]);
+                await setPaths([response.path, ...paths]);
+            } catch(error) {
+                console.error(error);
+                window.alert("Failed to upload image. Try renaming the file to something unique and try again.");
+                return;
+            }
+        }
+        else {
+            console.log("Did not have a file");
         }
     }
 
-    //console.log("Images:", images);
-    console.log("Paths before render:", paths);
+    if(loading)
+        return <p>Loading...</p>
     return (
         <>
             <BusinessNavBar/>
@@ -163,14 +167,12 @@ function BusinessManagementPicturesPage() {
                             <button className="btn" onClick={handleOutsideImgUpload}>Upload Image</button>
                         </div>
                         <div className='spread scroll'>
-                            {/* {images.map((source, index) => (
-                                <div key={index} className="img-with-button"> 
-                                    <img src={source}/>
-                                    <button className="btn" onClick={handleDelete(index)}>Delete</button>
-                                </div>
-                            ))} */}
                             {paths.map((source, index) => (
-                                <StorageImage key={index} alt="business photo" path={source}/>
+                                <div key={index} className="img-with-button"> 
+                                    {/* <img src={source}/> */}
+                                    <StorageImage alt="uploaded photo" path={source} bucket="AWSMBG4-private"/>
+                                    <button className="btn" onClick={() => handleDelete(index)}>Delete</button>
+                                </div>
                             ))}
                         </div>
                     </div>
