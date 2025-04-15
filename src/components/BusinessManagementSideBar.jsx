fe43d28b2d4fa066fbec1c6912abf073a2b68560
Chukwuemeka Ojih <emmy@chukwueekasmbp2.mynetworksettings.com>
2025-04-15 18:35:49 -0400

    revamped Business pipeline

--- src/components/BusinessManagementSideBar.jsx
+++ src/components/BusinessManagementSideBar.jsx
@@ -1,13 +1,13 @@
-import '../pages/BusinessManagementPage.css'
+import '../pages/BusinessManagementPage.css';
 
 function BusinessManagementSidebar() {
-    return (
-        <div className="sidebar">
-            <a href="">Business Information</a>
-            <a href="">Reviews</a>
-            <a href="">Photos</a>
-        </div>
-    );
+  return (
+    <div className="revamped-sidebar">
+      <a href="#" className="sidebar-link active">Business Information</a>
+      <a href="#" className="sidebar-link">Reviews</a>
+      <a href="#" className="sidebar-link">Photos</a>
+    </div>
+  );
 }
 
-export default BusinessManagementSidebar
+export default BusinessManagementSidebar;
